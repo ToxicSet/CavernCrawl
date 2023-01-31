2abf2767b904e5a9589fb3129b495dee311ed7f1
Logan Stigler <39494247+ToxicSet@users.noreply.github.com>
2023-01-31 12:53:37 -0800

    Starting Consumables

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,10 +6,24 @@ public class Item
 {
     public List<BaseStat> Stats { get; set; }
     public string ObjectSlug { get; set; }
+    public string Discription { get; set; }
+    public string ActionName { get; set; }
+    public string ItemName { get; set; }
+    public bool ItemModifier {get; set;}
 
     public Item(List<BaseStat> _Stats, string objectSlug)
     {
         this.Stats = _Stats;
         this.ObjectSlug = objectSlug;
+    }
+
+        public Item(List<BaseStat> _Stats, string objectSlug, string Discription, string ItemName, string ActionName, bool ItemModifier)
+    {
+        this.Stats = _Stats;
+        this.ObjectSlug = objectSlug;
+        this.Discription = Discription;
+        this.ActionName = ActionName;
+        this.ItemName = ItemName;
+        this.ItemModifier = ItemModifier;
     }
 }
